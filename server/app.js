93e10676fb0ff5b4c15091d66dbe20afee5d682e
Gongfan Zhang <gz74@students.waikato.ac.nz>
2025-07-25 17:39:30 +1200

    Refactor app.js: use async-await structure to replace the previous .then() structure; Add comments

--- server/app.js
+++ server/app.js
@@ -1,25 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "./models/index.js";
-import productRoutes from "./routes/product.route.js"
+import productRoutes from "./routes/product.route.js";
 
+// Load environment variable configurations, set server protocol, host, port
 dotenv.config();
-
-const app = express();
-app.use(express.json());
-
 const PROTOCOL = process.env.PROTOCOL || "http";
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 8000;
 
-app.use("/api/products", productRoutes);
+// Create an instance of the Express
+const app = express();
+app.use(express.json()); // Parse Json request body 
+app.use("/api/products", productRoutes); // Mount product route
 
-db.sequelize.sync().then(() => {
+// Start the server by defining an async function
+const startServer = async () => {
+  try {
+    await db.sequelize.sync(); // Synchronize Sequelize model with database
+
+    // Start the server and listen on the specified port
     app.listen(PORT, () => {
-            console.log(`server running successfully at ${PROTOCOL}://${HOST}:${PORT}`);
-        }
-    );
-}
+      console.log(
+        `server running successfully at ${PROTOCOL}://${HOST}:${PORT}`
+      );
+    });
+  } catch (error) {
+    console.error("failed to start", error);
+  }
+};
 
-);
-
+startServer();
