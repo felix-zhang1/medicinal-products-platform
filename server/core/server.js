a2e84d966b2ebe4b0d07b7a1a6e9829c438adda6
Gongfan Zhang <zhanggongfan2024@gmail.com>
2025-08-07 10:29:19 +1200

    import the new models and call the corresponding initialize functiong to initialize the models in database

--- server/core/server.js
+++ server/core/server.js
@@ -1,8 +1,17 @@
 import express from "express";
 import productRoutes from "../routes/product.route.js";
 import supplierRoutes from "../routes/supplier.route.js";
+
 import Product from "../models/product.model.js";
 import Supplier from "../models/supplier.model.js";
+import User from "../models/user.model.js";
+import Category from "../models/category.model.js";
+import CartItem from "../models/cart_item.model.js";
+import Favorite from "../models/favorite.model.js";
+import Order from "../models/order.model.js";
+import OrderItem from "../models/order_item.model.js";
+import Review from "../models/review.model.js";
+
 import sequelize from "../config/db.config.js";
 
 // instantiate the Express, load midwares, mount routes
@@ -16,6 +25,13 @@ app.use("/api/suppliers", supplierRoutes);
 function initializeModels(){
     Product.initialize(sequelize);
     Supplier.initialize(sequelize);
+    User.initialize(sequelize);
+    Category.initialize(sequelize);
+    CartItem.initialize(sequelize);
+    Favorite.initialize(sequelize);
+    Order.initialize(sequelize);
+    OrderItem.initialize(sequelize);
+    Review.initialize(sequelize);
 }
 
 // connect and initialize database (also sync models with database)
