831f26ebd3991c3511f6d5b21eb54be3d39236ad
Gongfan Zhang <zhanggongfan2024@gmail.com>
2025-08-14 20:39:49 +1200

    install the stripe library to implement real-world payment functionality

--- server/core/server.js
+++ server/core/server.js
@@ -37,71 +37,71 @@ app.use("/api/reviews", reviewRoutes);
 
 // initialize models
 function initializeModels() {
-    Product.initialize(sequelize);
-    Supplier.initialize(sequelize);
-    User.initialize(sequelize);
-    Category.initialize(sequelize);
-    CartItem.initialize(sequelize);
-    Favorite.initialize(sequelize);
-    Order.initialize(sequelize);
-    OrderItem.initialize(sequelize);
-    Review.initialize(sequelize);
+  Product.initialize(sequelize);
+  Supplier.initialize(sequelize);
+  User.initialize(sequelize);
+  Category.initialize(sequelize);
+  CartItem.initialize(sequelize);
+  Favorite.initialize(sequelize);
+  Order.initialize(sequelize);
+  OrderItem.initialize(sequelize);
+  Review.initialize(sequelize);
 }
 
 // define relations between models
 function defineModelRelations() {
-    // product relations
-    Product.belongsTo(Category, { foreignKey: "category_id" });
-    Category.hasMany(Product, { foreignKey: "category_id" });
+  // product relations
+  Product.belongsTo(Category, { foreignKey: "category_id" });
+  Category.hasMany(Product, { foreignKey: "category_id" });
 
-    Product.belongsTo(Supplier, { foreignKey: "supplier_id" });
-    Supplier.hasMany(Product, { foreignKey: "supplier_id" });
+  Product.belongsTo(Supplier, { foreignKey: "supplier_id" });
+  Supplier.hasMany(Product, { foreignKey: "supplier_id" });
 
-    Product.hasMany(CartItem, { foreignKey: "product_id" });
-    CartItem.belongsTo(Product, { foreignKey: "product_id" });
+  Product.hasMany(CartItem, { foreignKey: "product_id" });
+  CartItem.belongsTo(Product, { foreignKey: "product_id" });
 
-    Product.hasMany(Favorite, { foreignKey: "product_id" });
-    Favorite.belongsTo(Product, { foreignKey: "product_id" });
+  Product.hasMany(Favorite, { foreignKey: "product_id" });
+  Favorite.belongsTo(Product, { foreignKey: "product_id" });
 
-    Product.hasMany(Review, { foreignKey: "product_id" });
-    Review.belongsTo(Product, { foreignKey: "product_id" });
+  Product.hasMany(Review, { foreignKey: "product_id" });
+  Review.belongsTo(Product, { foreignKey: "product_id" });
 
-    Product.hasMany(OrderItem, { foreignKey: "product_id" });
-    OrderItem.belongsTo(Product, { foreignKey: "product_id" });
+  Product.hasMany(OrderItem, { foreignKey: "product_id" });
+  OrderItem.belongsTo(Product, { foreignKey: "product_id" });
 
-    // user relations
-    User.hasMany(CartItem, { foreignKey: "user_id" });
-    CartItem.belongsTo(User, { foreignKey: "user_id" });
+  // user relations
+  User.hasMany(CartItem, { foreignKey: "user_id" });
+  CartItem.belongsTo(User, { foreignKey: "user_id" });
 
-    User.hasMany(Favorite, { foreignKey: "user_id" });
-    Favorite.belongsTo(User, { foreignKey: "user_id" });
+  User.hasMany(Favorite, { foreignKey: "user_id" });
+  Favorite.belongsTo(User, { foreignKey: "user_id" });
 
-    User.hasMany(Review, { foreignKey: "user_id" });
-    Review.belongsTo(User, { foreignKey: "user_id" });
+  User.hasMany(Review, { foreignKey: "user_id" });
+  Review.belongsTo(User, { foreignKey: "user_id" });
 
-    User.hasMany(Order, { foreignKey: "user_id" });
-    Order.belongsTo(User, { foreignKey: "user_id" });
+  User.hasMany(Order, { foreignKey: "user_id" });
+  Order.belongsTo(User, { foreignKey: "user_id" });
 
-    // order relations
-    Order.hasMany(OrderItem, { foreignKey: "order_id" });
-    OrderItem.belongsTo(Order, { foreignKey: "order_id" });
+  // order relations
+  Order.hasMany(OrderItem, { foreignKey: "order_id" });
+  OrderItem.belongsTo(Order, { foreignKey: "order_id" });
 
-    // category sele-reference
-    Category.hasMany(Category, { as: "subcategories", foreignKey: "parent_id" });
-    Category.belongsTo(Category, { as: "parent", foreignKey: "parent_id" });
+  // category sele-reference
+  Category.hasMany(Category, { as: "subcategories", foreignKey: "parent_id" });
+  Category.belongsTo(Category, { as: "parent", foreignKey: "parent_id" });
 
 }
 
 // connect and initialize database (also sync models with database)
 async function initializeDatabase() {
-    try {
-        // initialize database and allow to change table structure automatically
-        await sequelize.sync({ alter: true });
-        console.log("database sync successfully");
+  try {
+    // initialize database and allow to change table structure automatically
+    await sequelize.sync({ alter: true });
+    console.log("database sync successfully");
 
-    } catch (error) {
-        console.error("database sync failed", error);
-    }
+  } catch (error) {
+    console.error("database sync failed", error);
+  }
 };
 
 export { initializeModels, defineModelRelations, initializeDatabase, app };
