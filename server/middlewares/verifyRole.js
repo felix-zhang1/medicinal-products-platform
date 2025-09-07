23180099e1ead5b73775fa3080b7d9787a032674
Gongfan Zhang <zhanggongfan2024@gmail.com>
2025-09-08 10:36:30 +1200

    add new function that support verify multiple roles

--- server/middlewares/verifyRole.js
+++ server/middlewares/verifyRole.js
@@ -1,11 +1,11 @@
+export function verifyRole(requiredRole) {
 
-
-export function verifyRole(requiredRole){
-    return (req, res, next) => {
-        if(!req.user || req.user.role !== requiredRole){
-            return res.status(403).json({message: "Insufficient permissions"});
-        }
-        next();
-    };
-
+  // unify the data type of requiredRole as an array
+  const allowed = Array.isArray(requiredRole) ? requiredRole : [requiredRole];
+  return (req, res, next) => {
+    if (!req.user || !allowed.includes(req.user.role)) {
+      return res.status(403).json({ message: "Insufficient permissions" });
+    }
+    next();
+  };
 }
