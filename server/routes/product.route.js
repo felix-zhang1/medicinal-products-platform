7f2d1334bd1f29883649518edeb297b84d3daeae
Gongfan Zhang <zhanggongfan2024@gmail.com>
2025-09-08 10:30:48 +1200

    add verify role and token logic in post methon, delete method, only allow the 'admin' role have permissions

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import ProductController from "../controllers/product.controller.js"
+import { verifyToken } from "../middlewares/verifyToken.js";
+import { verifyRole } from "../middlewares/verifyRole.js";
 
 const router = express.Router();
 
 // create a new product
-router.post("/", ProductController.createProduct);
+router.post("/", verifyToken, verifyRole("admin"), ProductController.createProduct);
 
 // get all products
 router.get("/", ProductController.getAllProducts);
@@ -13,9 +15,9 @@ router.get("/", ProductController.getAllProducts);
 router.get("/:id", ProductController.getProductById);
 
 // delete a product by id
-router.delete("/:id", ProductController.deleteProductById);
+router.delete("/:id", verifyToken, verifyRole("admin"), ProductController.deleteProductById);
 
 // update a product by id
-router.put("/:id", ProductController.updateProductById);
+router.put("/:id", verifyToken, verifyRole("admin"), ProductController.updateProductById);
 
 export default router;
