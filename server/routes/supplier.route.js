9670417fd934b1012feb563a2c88aed1caeb0859
Gongfan Zhang <zhanggongfan2024@gmail.com>
2025-09-08 10:50:40 +1200

    allow 'admin' and 'supplier' have permissions to create or update supplier, and only allow admin role to delete supplier

--- server/routes/supplier.route.js
+++ server/routes/supplier.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import supplierController from "../controllers/supplier.controller.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
+import { verifyRole } from "../middlewares/verifyRole.js";
 
 const router = express.Router();
 
-// create a new supplier
-router.post("/", supplierController.createSupplier);
+// create a new supplier ("admin", "supplier" have permissions)
+router.post("/", verifyToken, verifyRole(["admin","supplier"]), supplierController.createSupplier);
 
 // get all suppliers
 router.get("/", supplierController.getAllSuppliers);
@@ -13,9 +15,9 @@ router.get("/", supplierController.getAllSuppliers);
 router.get("/:id", supplierController.getSupplierById);
 
 // delete a supplier by id
-router.delete("/:id", supplierController.deleteSupplierById);
+router.delete("/:id", verifyToken, verifyRole("admin"), supplierController.deleteSupplierById);
 
-// update a supplier by id
-router.put("/:id", supplierController.updateSupplierById);
+// update a supplier by id, ("admin", "supplier" have permissions)
+router.put("/:id", verifyToken, verifyRole(["admin","supplier"]), supplierController.updateSupplierById);
 
 export default router;
