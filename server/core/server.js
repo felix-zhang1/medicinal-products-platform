552710c5d37df2e7e4ca1ff75088ca4f09702272
Gongfan Zhang <zhanggongfan2024@gmail.com>
2025-09-12 08:23:53 +1200

    create new directories (server/public/uploads) to store images; Expose the backend static resource directory (server/public/uploads) to the frontend.

--- server/core/server.js
+++ server/core/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import productRoutes from "../routes/product.route.js";
 import supplierRoutes from "../routes/supplier.route.js";
@@ -45,6 +46,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
+app.use("/uploads", express.static(path.join(process.cwd(), "server", "public", "uploads")));
 
 app.use("/api/products", productRoutes);
 app.use("/api/suppliers", supplierRoutes);
